5508eed272f09d931935e5709dc71b53135c54c3
Igor Sereda <sereda.igor.s@gmail.com>
2024-02-03 01:19:45 +0300

    Dynamic index added

--- bridge_indexer/hooks/on_synchronized.py
+++ bridge_indexer/hooks/on_synchronized.py
@@ -1,9 +1,13 @@
+from dipdup.exceptions import IndexAlreadyExistsError
 from dipdup.context import HookContext
-
-from bridge_indexer.handlers.tezos.on_head import update_commitment
 
 
 async def on_synchronized(
     ctx: HookContext,
 ) -> None:
     await ctx.execute_sql('on_synchronized')
+    try:
+        await ctx.add_index('tezos_head', 'tezos_head_index_template', values={})
+    except IndexAlreadyExistsError:
+        pass
+
